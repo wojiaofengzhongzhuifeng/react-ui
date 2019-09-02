85e0d9a8ce5b90224ff19ec1f4092bc99f920402
饶家俊 <raojiajun@bimwinner.com>
2019-09-02 19:00:13 +0800

    解决刷新之后，404 报错

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,5 +9,10 @@ module.exports = Object.assign({}, base, {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: 'example.html'
-		})]
+		})],
+	devServer: {
+		historyApiFallback: true,
+		contentBase: './',
+		hot: true
+	},
 })
