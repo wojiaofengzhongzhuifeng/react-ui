21399332f953b332a5e470716a39ef847b3baba6
饶家俊 <raojiajun@bimwinner.com>
2019-09-04 21:49:56 +0800

    使用 layout 组件构建 doc 框架

--- example.tsx
+++ example.tsx
@@ -4,29 +4,46 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import IconExample from './lib/icon/Icon.example';
 import DialogExample from './lib/dialog/Dialog.example';
 import LayoutExample from './lib/layout/Layout.example'
+import Layout from './lib/layout/Layout';
+import Header from './lib/layout/Header';
+import Content from './lib/layout/Content';
+import Footer from './lib/layout/Footer';
+import Sider from './lib/layout/Sider';
 
 const Example = ()=>{
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/Icon">Icon</Link>
-            </li>
-            <li>
-              <Link to="/dialog">Dialog</Link>
-            </li>
-            <li>
-              <Link to="/layout">Layout</Link>
-            </li>
-          </ul>
-        </nav>
+      <Layout style={{marginBottom: "10px"}}>
+        <Header>
+          icon
+        </Header>
+        <Layout>
+          <Sider>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/Icon">Icon</Link>
+                </li>
+                <li>
+                  <Link to="/dialog">Dialog</Link>
+                </li>
+                <li>
+                  <Link to="/layout">Layout</Link>
+                </li>
+              </ul>
+            </nav>
+          </Sider>
+          <Content>
+            <div>
+              <Route path="/Icon" exact component={IconExample} />
+              <Route path="/dialog" component={DialogExample} />
+              <Route path="/layout" component={LayoutExample} />
+            </div>
+          </Content>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
 
-        <Route path="/Icon" exact component={IconExample} />
-        <Route path="/dialog" component={DialogExample} />
-        <Route path="/layout" component={LayoutExample} />
-      </div>
     </Router>
   )
 };
