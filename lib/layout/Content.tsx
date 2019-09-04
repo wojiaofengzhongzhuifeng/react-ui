a96cb0a29fe096010e6b85d6d96c2faea498a448
饶家俊 <raojiajun@bimwinner.com>
2019-09-04 21:55:11 +0800

    添加 layout 相关组件class

--- lib/layout/Content.tsx
+++ lib/layout/Content.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { classes, scopeClass } from '../helpers/classes';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement>, React.Props<any>{
 
 }
 
+const sc = scopeClass('rao-layout');
+
 const Content: React.FunctionComponent<Props> = (props)=>{
   const {children, className} = props;
 
   return (
-    <div {...props} className={className}>{children}</div>
+    <div {...props} className={classes(sc('content'), className)}>{children}</div>
   )
 };
 
