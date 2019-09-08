56d02aee71c123e35c6c504cd50b7f69b13ad380
饶家俊 <raojiajun@bimwinner.com>
2019-09-08 22:22:43 +0800

    传入 table 的 DataSource 和 Column

--- lib/Table/Table.tsx
+++ lib/Table/Table.tsx
@@ -2,7 +2,20 @@ import * as React from 'react';
 import { scopeClass } from '../helpers/classes';
 import './style.scss';
 
+export interface DataSource {
+  key: string | number
+  [k: string]: any
+}
+
+export interface Column {
+  key: string | string
+  title: string
+  dataIndex: string
+}
+
 interface Props extends React.Props<any>{
+  dataSource: Array<DataSource>
+  columns: Array<Column>
 }
 
 const sc = scopeClass('rao-table');
@@ -10,6 +23,12 @@ const sc = scopeClass('rao-table');
 console.log(sc);
 
 const Table: React.FunctionComponent<Props> = (props)=> {
+  const {columns, dataSource} = props;
+
+  console.log(columns);
+
+  console.log(dataSource);
+
   return (
     <div>table</div>
   )
--- lib/icon/Icon.demo.tsx
+++ lib/icon/Icon.demo.tsx
@@ -2,7 +2,40 @@ import IconExample1 from './icon.example1';
 import React from 'react';
 import Demo from '../../demo';
 import Card from '../Card/Card';
-import Table from '../Table/Table';
+import Table, {DataSource, Column} from '../Table/Table';
+
+const IconDataSource: Array<DataSource> = [
+  {
+    key: 1,
+    arguments: 'className',
+    description: '通过输入 iconName，可以展示不同的 icon',
+    type: 'string',
+    defaultValue: '无',
+  }
+];
+
+const IconColumns: Array<Column> = [
+  {
+    title: '参数',
+    key: 'arguments',
+    dataIndex: 'arguments'
+  },
+  {
+    title: '描述',
+    key: 'description',
+    dataIndex: 'description'
+  },
+  {
+    title: '类型',
+    key: 'type',
+    dataIndex: 'type'
+  },
+  {
+    title: '默认值',
+    key: 'defaultValue',
+    dataIndex: 'defaultValue'
+  }
+];
 
 const IconDemo = () => {
   return (
@@ -29,7 +62,7 @@ const IconDemo = () => {
             SVGAttributes：本组件继承了 SVG 元素，拥有所有 SVG 元素属性
           </li>
         </ul>
-        <Table>test</Table>
+        <Table dataSource={IconDataSource} columns={IconColumns} />
       </Card>
     </div>
   );
