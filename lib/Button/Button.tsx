ed3ae346e9bb77e8373e38ea4c1ccb68f5ceb8c9
饶家俊 <raojiajun@bimwinner.com>
2019-09-13 14:52:47 +0800

    button props

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
 import { scopeClass } from '../helpers/classes';
+import { tuple } from '../_utils/types';
 
 const sc = scopeClass('rao-button');
 
 console.log(sc());
 
-const Button: React.FunctionComponent<any> = ()=> {
+const ButtonTypes = tuple('default', 'primary', 'link');
+type ButtonType = (typeof ButtonTypes)[number];
+
+const HTMLTypes = tuple('submit', 'button');
+type HTMLType = (typeof HTMLTypes)[number];
+
+interface NativeButtonProps extends Omit<React.ButtonHTMLAttributes<any>, 'type'>{
+  type?: ButtonType
+  icon?: string
+  loading?: boolean
+  className?: string
+  htmlType?: HTMLType
+  onClick?: React.MouseEventHandler
+}
+
+interface AnchorButtonProps extends Omit<React.ButtonHTMLAttributes<any>, 'type'>{
+  type?: ButtonType
+  icon?: string
+  loading?: boolean
+  className?: string
+  href?: string
+  onClick?: React.MouseEventHandler
+}
+
+interface ButtonProps extends NativeButtonProps, AnchorButtonProps{}
+
+const Button: React.FunctionComponent<ButtonProps> = (props)=> {
+  const {type} = props;
   return (
     <button>button</button>
   )
