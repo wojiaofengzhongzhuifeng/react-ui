1358b93fec2bcf19533f368a6b8e22b5844296a2
饶家俊 <raojiajun@bimwinner.com>
2019-09-13 14:44:07 +0800

    init button

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -1,27 +1,15 @@
 import * as React from 'react';
 import { scopeClass } from '../helpers/classes';
-import { tuple } from '../_utils/types';
-
-const Types = tuple('primary', 'link');
-export type type = (typeof Types)[number];
-
-interface Props extends React.Props<any>{
-  type?: type,
-}
 
 const sc = scopeClass('rao-button');
 
 console.log(sc());
 
-const Button: React.FunctionComponent<Props> = (props)=> {
-  const {type} = props;
+const Button: React.FunctionComponent<any> = ()=> {
   return (
-    <button>button{type}</button>
+    <button>button</button>
   )
 };
 
-Button.defaultProps = {
-  type: 'primary'
-};
 
 export default Button;
