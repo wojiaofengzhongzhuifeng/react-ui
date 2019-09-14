1d11db7d26a8c4ce9de3818a2c3b94ecad7a3695
饶家俊 <raojiajun@bimwinner.com>
2019-09-14 17:42:51 +0800

    修改第一个 demo

--- lib/Dialog/Dialog.example1.tsx
+++ lib/Dialog/Dialog.example1.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import Dialog, {alert} from './Dialog';
+import Dialog from './Dialog';
 import { useState } from 'react';
+import Button from '../Button';
 
 const DialogExample1: React.FunctionComponent = ()=> {
   const [visible, setVisible] = useState<Boolean>(false);
@@ -9,42 +10,49 @@ const DialogExample1: React.FunctionComponent = ()=> {
     setVisible(!visible);
   };
 
-  const test = ()=>{
-    const test1 = ()=>{
-      console.log(2);
-    };
-    const closeModalFun = alert({
-      text: (
-        <div>
-          <h1>test123</h1>
-          <p>nihaifdsa</p>
-          <button onClick={()=>{closeModalFun()}}>关闭这个 modal </button>
-        </div>
-      ),
-      buttons: [
-        <button onClick={()=>{console.log(1);}}>test1</button>,
-        <button onClick={test1}>test1</button>,
-      ]
-    })
+  // const test = ()=>{
+  //   const test1 = ()=>{
+  //     console.log(2);
+  //   };
+  //   const closeModalFun = alert({
+  //     text: (
+  //       <div>
+  //         <h1>test123</h1>
+  //         <p>nihaifdsa</p>
+  //         <button onClick={()=>{closeModalFun()}}>关闭这个 modal </button>
+  //       </div>
+  //     ),
+  //     buttons: [
+  //       <button onClick={()=>{console.log(1);}}>test1</button>,
+  //       <button onClick={test1}>test1</button>,
+  //     ]
+  //   })
+  // };
+
+  const handleClickCancel = ()=>{
+    console.log('点击了取消');
+  };
+
+  const handleClickConfirm = ()=>{
+    console.log('点击了确认');
   };
 
   return (
     <div>
-      <h1>Dialog</h1>
       <Dialog
         visible={visible}
         buttons={
           [
-            <button>test</button>,
-            <button>test123321</button>
+            <Button onClick={handleClickCancel}>取消</Button>,
+            <Button onClick={handleClickConfirm}>确认</Button>
           ]
         }
         handleClickXOrMask={()=>{setVisible(!visible)}}
         closeOnClickMask={false}
       >
-        hi this is me
+        dialog 内容
       </Dialog>
-      <button onClick={handleClickButton}>打开 dialog</button>
+      <Button onClick={handleClickButton}>打开 dialog 的第一种方法</Button>
     </div>
   );
 };
