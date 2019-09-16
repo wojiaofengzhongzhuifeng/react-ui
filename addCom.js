8e5802228e645e5a8552e4b119bd9b6fb10d3c0a
饶家俊 <raojiajun@bimwinner.com>
2019-09-16 17:43:49 +0800

    完成 demo 文件

--- addCom.js
+++ addCom.js
@@ -12,63 +12,21 @@ const buttonDemoString = `
 import * as React from 'react';
 import Card from '../Card/Card';
 import Demo from '../../demo';
-import ButtonExample1 from './Button.example1';
+import ${componentName}Example1 from './${componentName}.example1';
 import Table, { Column, DataSource } from '../Table/Table';
-import Button from './Button';
+import ${componentName} from './${componentName}';
 
-const ButtonDataSource: Array<DataSource> = [
+const dataSource: Array<DataSource> = [
   {
     key: 1,
     arguments: 'type',
     description: '指定 button 的类型，根据类型，样式不同，可选类型有「\\'default\\', \\'primary\\', \\'link\\'」',
     type: 'string',
     defaultValue: '无',
-  },
-  {
-    key: 2,
-    arguments: 'icon',
-    description: '输入 iconName，可向 button 左侧添加 iconName 对应 icon',
-    type: 'string',
-    defaultValue: '无',
-  },
-  {
-    key: 3,
-    arguments: 'loading',
-    description: 'button 左侧的 loading 状态，完全由用户指定 loading 状态',
-    type: 'boolean',
-    defaultValue: 'false',
-  },
-  {
-    key: 4,
-    arguments: 'icon',
-    description: '输入 iconName，可向 button 左侧添加 iconName 对应 icon',
-    type: 'string',
-    defaultValue: '无',
-  },
-  {
-    key: 5,
-    arguments: 'href',
-    description: 'button 作为跳转按钮，即将跳转的地址',
-    type: 'string',
-    defaultValue: '无',
-  },
-  {
-    key: 6,
-    arguments: 'target',
-    description: '如何打开跳转地址',
-    type: 'string',
-    defaultValue: '无',
-  },
-  {
-    key: 7,
-    arguments: '...reset',
-    description: '本组件继承自 React.ButtonHTMLAttributes， 拥有 React.ButtonHTMLAttributes 所有属性',
-    type: 'string',
-    defaultValue: '无',
   }
 ];
 
-const ButtonColumns: Array<Column> = [
+const columns: Array<Column> = [
   {
     title: '参数',
     key: 'arguments',
@@ -91,42 +49,40 @@ const ButtonColumns: Array<Column> = [
   }
 ];
 
-const ButtonDemo = () => {
+const ${componentName}Demo = () => {
   return (
     <div>
       <Card>
         <h2>概述</h2>
-        基于 HTML 原生元素封装的 Button 组件
+        test
       </Card>
 
       <Card>
         <h2>使用示例</h2>
-        <Demo code={require('!!raw-loader!./Button.example1.tsx').default}>
-          <ButtonExample1/>
+        <Demo code={require('!!raw-loader!./${componentName}.example1.tsx').default}>
+          <${componentName}Example1/>
         </Demo>
       </Card>
 
       <Card>
         <h2>API</h2>
-        <Table dataSource={ButtonDataSource} columns={ButtonColumns} />
+        <Table dataSource={dataSource} columns={columns} />
       </Card>
 
       <Card>
         <h2>总结</h2>
-        <Button type='link' href='https://github.com/wojiaofengzhongzhuifeng/study/blob/master/blog/react轮子/11Button.md' target='_blank'>连接</Button>
       </Card>
     </div>
   );
 };
 
-export default ButtonDemo;
-
+export default ${componentName}Demo;
 `
 
 // 创建 Tab 文档
 fs.mkdir(mkdirPath, null, (err)=>{
 	if(!err){
-		fs.writeFile(`${mkdirPath}/style.scss`, "test", (err)=>{
+		fs.writeFile(`${mkdirPath}/${componentName}.demo.tsx`, buttonDemoString, (err)=>{
 			console.log(err);
 		})
 	}
