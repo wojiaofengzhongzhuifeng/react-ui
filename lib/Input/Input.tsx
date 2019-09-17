14db46ff18c60b9202164be27b95c7e334379c71
饶家俊 <raojiajun@bimwinner.com>
2019-09-17 11:39:54 +0800

    InputProps props

--- lib/Input/Input.tsx
+++ lib/Input/Input.tsx
@@ -7,11 +7,16 @@ const sc = scopeClass('rao-input');
 
 console.log(sc);
 
-interface InputProps {}
+interface InputProps extends React.HTMLAttributes<InputProps>{
+  defaultValue: string
+  disabled: boolean
+  value: string
+  onPressEnter: React.KeyboardEventHandler
+}
 
 const Input: React.FunctionComponent<InputProps> = (props)=> {
   return (
-		<div>Input</div>
+		<input />
   )
 };
 
