9c6f2bb92bd402f3594422950d1f1ce73d9b0a04
饶家俊 <raojiajun@bimwinner.com>
2019-09-22 10:46:56 +0800

    flatten helper function

--- lib/helpers.tsx
+++ lib/helpers.tsx
@@ -2,5 +2,19 @@ export const isEmptyObj: (p1: any)=>boolean = (obj)=>{
   return Object.keys(obj).length === 0;
 };
 
+export const flatten: (p1: Array<any>)=>Array<any> = (array)=>{
+  const resultArray: Array<any>= [];
+
+  array.forEach((item)=>{
+    if(item instanceof Array){
+      resultArray.push(...item);
+    } else {
+      resultArray.push(item);
+    }
+  });
+
+  return resultArray;
+};
+
 
 
