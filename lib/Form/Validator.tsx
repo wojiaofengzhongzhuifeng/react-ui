8b1ff12f3c1bc4e075a1f9a8bf8a503fc93b281d
饶家俊 <raojiajun@bimwinner.com>
2019-09-22 09:24:03 +0800

    validator 重构代码

--- lib/Form/Validator.tsx
+++ lib/Form/Validator.tsx
@@ -15,29 +15,28 @@ export type Errors = PureErrors | null
 
 export const validator:(p1: FormData, p2: Array<Rule>)=>Errors = (formData, rules)=>{
   const errors: Errors = {};
+
+  const addError = (formName: string, errorMessage: string)=>{
+    if(!errors[formName]){
+      errors[formName] = [];
+    }
+    errors[formName].push(errorMessage);
+  };
+
   rules.map((rule)=>{
     // @ts-ignore
     const formName: string = rule.name;
 
     if(rule.isRequire && formData[formName] === ''){
-      if(!errors[formName]){
-        errors[formName] = [];
-      }
-      errors[formName].push('必填');
+      addError(formName, '必填')
     }
 
     if(rule.minLength && (formData[formName].length <= rule.minLength)){
-      if(!errors[formName]){
-        errors[formName] = [];
-      }
-      errors[formName].push('太短');
+      addError(formName, '太短')
     }
 
     if(rule.maxLength && (formData[formName].length >= rule.maxLength)){
-      if(!errors[formName]){
-        errors[formName] = [];
-      }
-      errors[formName].push('太长');
+      addError(formName, '太长')
     }
   });
   return isEmptyObj(errors) ? null : errors;
