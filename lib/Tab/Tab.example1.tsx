180237351f6f6b0752514efa94bca246ac8df137
饶家俊 <471412450@qq.com>
2021-04-30 17:50:47 +0800

    完成最简单的 tab 组件

--- lib/Tab/Tab.example1.tsx
+++ lib/Tab/Tab.example1.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
-import Tab from './Tab';
+import {Tab, TabPane} from './Tab';
+import {ReactText, useState} from "react";
 
 const TabExample1: React.FunctionComponent = ()=>{
+  const [tabKey, setTabKey] = useState<ReactText>('1');
+
+  const onChange = (e: any, clickedTabPaneKey: ReactText)=>{
+    setTabKey(clickedTabPaneKey)
+  }
   return (
     <div>
-      <Tab>Tab</Tab>
+      <Tab activeKey={tabKey} onChange={onChange}>
+        <TabPane key="1" tabName={'体育'}>
+          体育内容
+        </TabPane>
+        <TabPane key="2" tabName={'科技'}>
+          科技内容
+        </TabPane>
+        <TabPane key="3" tabName={'生活'}>
+          生活内容
+        </TabPane>
+      </Tab>
     </div>
   )
 };
--- lib/Tab/Tab.tsx
+++ lib/Tab/Tab.tsx
@@ -1,17 +1,66 @@
 import * as React from 'react';
 import { scopeClass } from '../helpers/helps';
+import {ReactElement, ReactText} from "react";
+import './style.scss';
 
-interface Props extends React.Props<any>{
+interface TabProps extends React.Props<any>{
+  activeKey: ReactText, // 当前选择的 key
+  onChange: (e: React.MouseEvent<HTMLDivElement, MouseEvent>, clickedTabPaneKey: ReactText)=>void, // 选择 tabPab
+  children: Array<ReactElement>
+}
+interface TabPaneProps extends React.Props<any>{
+  tabName: ReactText, // tabPane 名称
+  key: ReactText, // tabPane 唯一标识符
 }
 
 const sc = scopeClass('rao-tab');
 
-const Tab: React.FunctionComponent<Props> = ()=> {
+export const Tab: React.FunctionComponent<TabProps> = (
+  {
+    children,
+    activeKey,
+    onChange
+  })=> {
+  const onClickPaneHeader = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, clickedTabPaneKey: ReactText)=>{
+    onChange(e, clickedTabPaneKey)
+  }
   return (
     <div className={sc()}>
-      tab
+      <div className={sc('pane-header-ct')}>
+        {children.map((tabPane)=>{
+          const tabPaneKey = tabPane.key as ReactText;
+          return (
+            <div
+              key={tabPaneKey}
+              onClick={(e)=>{onClickPaneHeader(e, tabPaneKey)}}
+              className={`${sc('pane-header')} ${tabPaneKey === activeKey ? 'active' : ''}`}
+            >
+              {tabPane && tabPane.props.tabName}
+            </div>
+          )
+        })}
+      </div>
+
+      <div className={sc('pane-content-ct')}>
+        {children.map((tabPane)=>{
+          if(tabPane.key == activeKey){
+            return (
+              <div key={tabPane.key}>{tabPane && tabPane.props.children}</div>
+            )
+          } else {
+            return null
+          }
+        })}
+      </div>
+
     </div>
   )
 };
 
-export default Tab;
+export const TabPane: React.FunctionComponent<TabPaneProps> = ({tabName, key, children})=>{
+  return (
+    <div className={sc('pane')} key={key}>
+    </div>
+  )
+}
+
