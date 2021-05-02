fad8d50f9ebd835c1bbd55bf3676abff4c45350b
饶家俊 <471412450@qq.com>
2021-05-02 11:30:58 +0800

    使用函数(非组件)递归渲染树形数据,但是遗留一些小问题

--- lib/Tree/Tree.tsx
+++ lib/Tree/Tree.tsx
@@ -12,10 +12,61 @@ console.log(sc);
 interface TreeProps {
   sourceData: TreeItem[]
 }
+interface TreeItemProps1 {
+  sourceData: TreeItem
+}
+interface TreeItemProps2{
+  key: string,
+  value: string,
+  children?: TreeItem[]
+}
+const TreeItem1: React.FunctionComponent<TreeItemProps1> = (props)=>{
+  console.log('props', props);
+  return (
+    <div key={props.sourceData.key}>
+      {props.sourceData.value}
+      {props.sourceData.children && props.sourceData.children.map((treeItem)=>{
+        return <TreeItem1 sourceData={treeItem}/>
+      })}
+    </div>
+  )
+}
+const TreeItem2: React.FunctionComponent<TreeItemProps2> = (props)=>{
+  const {value, children} = props;
+  console.log('props', props);
+  return (
+    <div key={value}>
+      {value}
+      {children && children.map((treeItem)=>{
+        console.log(treeItem);
+        return <TreeItem2 {...treeItem}/>
+      })}
+    </div>
+  )
+}
+
+const TreeItem3 = (item: TreeItem) => {
+  const {key, value, children} = item;
+
+  return <div key={key}>
+    {value}
+    {children && children.map(sub => {
+      return TreeItem3(sub);
+    })}
+  </div>;
+};
 
 const Tree: React.FunctionComponent<TreeProps> = (props)=> {
   return (
-		<div>{JSON.stringify(props.sourceData)}</div>
+		<div>
+      {
+        props.sourceData && props.sourceData.map((treeItem)=>{
+          // return <TreeItem1 sourceData={treeItem}/>
+          // return <TreeItem2 {...treeItem}/>
+          return TreeItem3(treeItem)
+        })
+      }
+    </div>
   )
 };
 
